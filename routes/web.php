ecb0ce6a33d4e7632de88d21693b91d28586f131
shafirasalim <shafira03excellent@gmail.com>
2026-06-18 05:24:59 +0700

    feat: install Laravel Breeze for authentication

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,20 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ProductController;
-use App\Http\Controllers\BranchController;
-use App\Http\Controllers\StockController;
-use App\Http\Controllers\UserController;
-use App\Http\Controllers\StockMovementController;
 
-Route::resource('stock-movements', StockMovementController::class);
+Route::get('/', function () {
+    return view('welcome');
+});
 
-Route::resource('products', ProductController::class);
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::resource('branches', BranchController::class);
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
 
-Route::resource('stocks', StockController::class);
-
-Route::resource('users', UserController::class);
+require __DIR__.'/auth.php';
